da6eaea4539ce9e64ffdb92b99a2552b2d0e5e8d
John Zhang <iamjz210@gmail.com>
2026-08-20 11:13:18 +0800

    chore: bump version to 3.0.2

--- cmd/root.go
+++ cmd/root.go
@@ -32,7 +32,7 @@ import (
 // stdinReader is the reader used for interactive prompts. Tests can replace it.
 var stdinReader io.Reader = os.Stdin
 
-var Version = "3.0.1"
+var Version = "3.0.2"
 
 var updateChecker *update.Checker
 
